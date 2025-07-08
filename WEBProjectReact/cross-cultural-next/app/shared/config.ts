cdeeec1df9a44b8d70e7ee1c9e6d754cc2e0bfa3
shadi159 <shadikeesha@gmail.com>
2025-07-08 10:34:01 +0300

    fix P2P

--- WEBProjectReact/cross-cultural-next/app/shared/config.ts
+++ WEBProjectReact/cross-cultural-next/app/shared/config.ts
@@ -1,5 +1,5 @@
 export const API = process.env.NEXT_PUBLIC_API || '';
-export const P2P_SIGNALING_URL = process.env.NEXT_PUBLIC_P2P_SIGNALING_URL || 'ws://localhost:8080';
+export const P2P_SIGNALING_URL = "wss://0.peerjs.com/peerjs/0.6.1/"; // PeerJS public server
 export const P2P_CONFIG = {
   MAX_PARTICIPANTS: 4,
   RTC_CONFIGURATION: {
